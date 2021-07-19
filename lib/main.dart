976183ec883469f14e0f9b58c9c7b3ba3d58d6c7
harshanaguru360 <harshanaguru360@gmail.com>
2021-07-19 17:46:50 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,136 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:project/welcome.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'signup.dart';
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'LayoutBasic',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
-      home: LayoutBasics(),
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/signup': (BuildContext context) => new SignupPage(),
+        '/login': (BuildContext context) => new MyApp(),
+        '/welcome': (BuildContext context) => new MyLoginPage()
+      },
+      home: new MyHomePage(),
     );
   }
 }
 
-class LayoutBasics extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
+    return new Scaffold(
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
           Container(
-            color: Colors.red,
-            width: 1000,
-            height: 50,
-            child: Center(
-              child: Text('hii'),
-            ),
-          ),
-          SizedBox(height: 50), // code for three boxes in a row
-          Container(
-              child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                color: Colors.red,
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: Text('hii'),
-                ),
-              ),
-              Container(
-                color: Colors.red,
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: Text('hii'),
-                ),
-              ),
-              Container(
-                color: Colors.red,
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: Text('hii'),
-                ),
-              )
-            ],
-          )),
-          SizedBox(
-            height: 150,
-          ), //code for strip 2
-          Container(
-            color: Colors.grey,
-            width: 1000,
-            height: 50,
-            child: Center(
-              child: Text('hi'),
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ), //code for strip 3
-          Container(
-            color: Colors.grey,
-            width: 1000,
-            height: 50,
-            child: Center(
-              child: Text('hi'),
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ), //code for two boxes in coloumn
-
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
+            child: Stack(
+              children: <Widget>[
                 Container(
-                  color: Colors.red,
-                  width: 70,
-                  height: 70,
-                  child: Center(
-                    child: Text('hii'),
+                  padding: const EdgeInsets.fromLTRB(50, 50, 0, 0),
+                  child: Text(
+                    'CRUX FLUTTER',
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.greenAccent),
                   ),
                 ),
                 Container(
-                  child: Center(
-                    child: Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Container(
-                        height: 120.0,
-                        width: 120.0,
-                        color: Colors.red,
-                        child: Text('hii'),
-                      ),
-                    ),
+                  padding: EdgeInsets.fromLTRB(35.0, 90.0, 0, 0),
+                  child: Text(
+                    'SUMMER GROUP',
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.greenAccent),
                   ),
                 )
               ],
             ),
           ),
           Container(
-            child: Center(
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Container(
-                  height: 70.0,
-                  width: 70.0,
-                  color: Colors.red,
-                  child: Text('hii'),
+            padding: EdgeInsets.only(top: 35, left: 20, right: 20),
+            child: Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "ID Number",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(top: 14),
+                      hintText: 'Please enter your BITS ID Number',
+                      border: OutlineInputBorder(),
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black)),
+                ),
+                SizedBox(height: 50),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Password",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(top: 14),
+                      hintText: 'Please enter your password',
+                      border: OutlineInputBorder(),
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black)),
+                ),
+                SizedBox(height: 50),
+                Container(
+                  height: 40,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.greenAccent,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Center(
+                        child: Text(
+                          'LOG IN',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 15, left: 20),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot password',
+                      style: TextStyle(
+                          color: Colors.greenAccent,
+                          fontFamily: 'Monteserrat',
+                          decoration: TextDecoration.underline),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Don't have an account",
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
                 ),
               ),
-            ),
+              SizedBox(width: 5),
+              InkWell(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/signup');
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
           )
-        ],
-      ),
-    );
+        ]));
   }
 }
